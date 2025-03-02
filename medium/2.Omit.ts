bd1196cab224c311d265c1b24deb8a4c2c570696
Jinyoung Sung <sjy021621@gmail.com>
2025-03-03 04:43:29 +0900

    2.Omit

---
+++ medium/2.Omit.ts
@@ -0,0 +1,15 @@
+type MyOmit<T extends {}, K extends string> = {
+  [A in Exclude<keyof T, K>]: T[A];
+};
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+type TodoPreview = MyOmit<Todo, "description" | "title">;
+
+const todo: TodoPreview = {
+  completed: false,
+};
